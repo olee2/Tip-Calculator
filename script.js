848ecde391c55e5010551199068432aa99dee82d
olee2 <65301179+olee2@users.noreply.github.com>
2022-01-27 18:02:21 +0100

    Update index, script, styles

--- script.js
+++ script.js
@@ -30,15 +30,63 @@ function percentage(){
     }
 }
 
+function people() {
+
+    let people = document.querySelector("#people"),
+        error = document.querySelector(".error"),
+        num = parseInt(people.value);
+        
+    if(num === 0){
+        error.innerHTML = "Can't be Zero";
+        people.classList.replace("number", "number-error")
+        return 0;
+
+    }else if(isNaN(num)){
+        people.classList.replace("number-error", "number")
+        error.innerHTML = "";
+        return 0;
+    }
+    people.classList.replace("number-error", "number")
+    error.innerHTML = "";
+    people.value = num;
+
+    return num;
+    
+}
+
+function billSum(){
+
+    let bill = document.querySelector("#bill");
+    let num = Number(bill.value);
+
+    if(num <= 0 || isNaN(num)){
+        bill.value = 0;
+        console.log(num);
+        return 0;
+    }
+    bill.value = num;
+    return num;
+}
+
 function calculate(){
-    let bill = document.querySelector("#bill").value,
+    let bill = billSum(),
+        numPeople = people(),
         tip = percentage(),
-        numPeople = document.querySelector("#people").value, 
-        tipPerson = (bill * tip) / numPeople;
+        tipPerson = (bill * tip) / numPeople,
         totalPerson = bill / numPeople + tipPerson;
 
+    if(tipPerson === Infinity || isNaN(tipPerson)){
+        document.querySelector("#tipPerson").innerHTML = "$0.00";
+        document.querySelector("#totalPerson").innerHTML = "$0.00";
+    }else{
         document.querySelector("#tipPerson").innerHTML = `$${tipPerson.toFixed(2)}`;
         document.querySelector("#totalPerson").innerHTML = `$${totalPerson.toFixed(2)}`;
+    }
+    
+
+   
+
+
 }
 
 let radios = document.querySelectorAll("input[type='radio']");
